bd65bd73fcfd4898c35ab810907a72a3dbf6560d
Denisse <yuleiditho@gmail.com>
2025-04-13 20:15:44 -0600

    docs: add base script to resolve the problem

---
+++ 05-Sliding-Window/worldTool/slidingWindowProject.js
@@ -0,0 +1,23 @@
+function findLongestWord(text) {
+    //TODO: Dividir el texto en palabras y almacenarlas en una variable
+    // const words = ...
+
+    let longestWord = ''; // Inicializar la palabra más larga
+
+    // TODO: Recorrer el arreglo de palabras con un ciclo
+    // for (...) {
+        // TODO: Comparar la longitud de la palabra actual con la más larga
+        // if (...) {
+            // Actualizar la palabra más larga
+            // longestWord = ...;
+        // }
+    // }
+
+    // TODO: Retornar la palabra más larga encontrada
+    // return ...;
+}
+
+// Ejemplo de uso
+const text = "JavaScript es un lenguaje de programación increíble para aprender.";
+// TODO: Llama a la función y muestra el resultado
+console.log(findLongestWord(text)); // Resultado esperado: "programación"
