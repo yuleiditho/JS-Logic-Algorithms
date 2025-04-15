01e60295138e02c81e7e7b380e775d121ace4bcf
Denisse <yuleiditho@gmail.com>
2025-04-15 13:59:10 -0600

    feat: Implements recursive gift search

--- 06-Recursive-Backtracking/toDoList/recursionAndBacktracking.js
+++ 06-Recursive-Backtracking/toDoList/recursionAndBacktracking.js
@@ -1,26 +1,28 @@
-// Copy code
 // Lista de regalos
 const gifts = ["Muñeca", "Carro de juguete", "Rompecabezas", "Lego", "Pelota"];
 
-// TODO: Completa esta función para que busque recursivamente el regalo en la lista
+
 function findGift(gifts, giftName, index = 0) {
-    // Caso base: Si llegamos al final de la lista
-    if (index === gifts.length) {
+  // Caso base 1: Si llegamos al final de la lista
+    if(index === gifts.length) {
         return `${giftName} no está en la lista.`;
     }
-  // TODO: Incluye el caso base donde se llega al final de la lista.
-  // TODO: Incluye el caso base donde se encuentra el regalo.
-  // TODO: Realiza la llamada recursiva para seguir buscando el regalo.
+    //Caso base 2: Si encuentra el regalo.
+    if(gifts[index] === giftName){
+        return `${giftName} está en la posición ${index}`;
+    }
+    //Llamada recursiva: continuar buscando en la siguiete posición
+    return findGift(gifts, giftName, index + 1);
 }
+
 // Casos de ejemplo:
-// Llama a la función con los datos de entrada y muestra el resultado en la consola.
-// Llama a la función y prueba con diferentes regalos
 let giftToFind = "Lego";
 console.log(findGift(gifts, giftToFind));
 // Salida esperada:
 // "Lego está en la posición 3."
 
 // Caso cuando el regalo no está en la lista
-giftToFind = "Camión";
+let giftToFind2 = "Camión";
+console.log(findGift(gifts, giftToFind2));
 // Salida esperada:
 // "Camión no está en la lista."
